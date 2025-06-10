21c9cddd49f6b631b66127fdbba93edb0a2e4768
Amirul <amirulal73@gmail.com>
2025-06-10 23:08:39 +0800

    Added delete task method

--- laravel-api/app/Http/Controllers/Api/V1/TaskController.php
+++ laravel-api/app/Http/Controllers/Api/V1/TaskController.php
@@ -53,8 +53,10 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
