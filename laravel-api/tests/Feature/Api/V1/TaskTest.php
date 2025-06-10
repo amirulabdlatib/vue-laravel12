c469413879adc6373187400b0e663a3ee9e15bc3
Amirul <amirulal73@gmail.com>
2025-06-10 15:16:07 +0800

    Added feature test on get all and get a task

---
+++ laravel-api/tests/Feature/Api/V1/TaskTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use Tests\TestCase;
+use App\Models\Task;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TaskTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_user_can_get_list_tasks()
+    {
+        // Arrange
+        Task::factory()->count(5)->create();
+
+        // Act
+        $response = $this->getJson('/api/v1/tasks');
+
+        // Assert
+        $response->assertOk();
+
+        $response->assertJsonCount(5,'data');
+
+        $response->assertJsonStructure([
+            'data'=>[
+                        ['id','name','is_completed']
+                ]
+            ]);
+    }
+
+    public function test_user_can_get_single_task()
+    {
+        // Arrange
+        $task = Task::factory()->create();
+
+        // Act
+        $response = $this->getJson('/api/v1/tasks/'. $task->id);
+
+        // Assert
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'data'=>['id','name','is_completed']
+        ]);
+
+        $response->assertJson([
+            'data'=>[
+                    'id'=>$task->id,
+                    'name'=>$task->name,
+                    'is_completed'=>$task->is_completed
+                ]
+            ]);
+    }
+
+}
